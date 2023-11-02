0bffef40e2a34e344c29ee2410259ba6fe5ab4d6
leah <leah@pronounmail.com>
2023-11-02 00:45:28 +0000

    innit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import "@fontsource-variable/piazzolla/opsz.css";
+    import "@fontsource-variable/piazzolla/opsz-italic.css";
+    import "../app.css";
+    import Navigation from "$lib/Navigation.svelte";
+</script>
+
+<Navigation />
+<main>
+    <slot />
+</main>
+
+<style>
+    main {
+        max-width: 64rem;
+        margin: 2rem auto;
+    }
+</style>
